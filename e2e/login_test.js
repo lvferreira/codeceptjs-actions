6d84f50b1fd3c1bac66d77a945323c9f021f1d41
Leandro Viola <leandro.vf07@gmail.com>
2023-05-19 16:32:30 -0300

    hotfix

--- e2e/login_test.js
+++ e2e/login_test.js
@@ -17,7 +17,7 @@ Scenario('usuário não existe', ({ I }) => {
 
 Scenario('senha incorreta', ({ I }) => {
     I.login('qa', 'teste')
-    I.toast('Oops! Credencial inválida :(')
+    I.toast('Oops! Credenciais inválidas :(')
 })
 
 Scenario('com sucesso', ({ I }) => {
